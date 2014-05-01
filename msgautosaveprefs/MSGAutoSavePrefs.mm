92553c0157dc27a1160a8e955b7b3fa7588178b8
sharedRoutine <appleidmail@aol.com>
2014-05-01 15:05:03 +0200

    Add Video Settings

--- msgautosaveprefs/MSGAutoSavePrefs.mm
+++ msgautosaveprefs/MSGAutoSavePrefs.mm
@@ -17,8 +17,19 @@
 }
 
 -(NSArray *)getValues:(PSSpecifier *)spec {
-	return @[@0.05,@0.1,@0.15,@0.20,@0.25,@0.30,@0.35,@0.40,@0.45,@0.50,@0.55,@0.60,@0.65,@0.70,@0.75,@0.80,@0.85,@0.90,@0.95,@1];
+	return @[@5,@10,@15,@20,@25,@30,@35,@40,@45,@50,@55,@60,@65,@70,@75,@80,@85,@90,@95,@100];
 }
+
+-(NSArray *)getVideoSizeTitles:(PSSpecifier *)spec {
+    
+    return @[@"640x480",@"960x540",@"1280x720",@"1920x1080",@"Do not resize Videos"];
+}
+
+-(NSArray *)getVideoSizeValues:(PSSpecifier *)spec {
+    
+    return @[@0,@1,@2,@3,@-1];
+}
+
 @end
 
 // vim:ft=objc
